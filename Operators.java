c321c9ea45e9e7edd5503296117267162d8b865f
anikateagrawal <anikateag7316@gmail.com>
2025-07-05 08:14:08 +0530

    commit

---
+++ Operators.java
@@ -0,0 +1,9 @@
+public class Operators {
+    public static void main(String[] args) {
+        System.out.println(10+20);
+        System.out.println(20-10);
+        System.out.println(10*20);
+        System.out.println(20/5);
+        System.out.println(20%3);
+    }
+}
