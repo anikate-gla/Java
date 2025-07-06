7a621066a5e359d7d093f395975a2cb51f8d2ba1
anikateagrawal <anikateag7316@gmail.com>
2025-07-06 12:47:48 +0530

    commit

---
+++ Second_Program.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Second_Program {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        // data types
+        //Integer types
+        byte b=sc.nextByte();
+        short sh=sc.nextShort();
+        int n=sc.nextInt();
+        long l=sc.nextLong();
+
+        //Floating point
+        float f=sc.nextFloat();
+        double d=sc.nextDouble();
+
+        // String
+        String s=sc.next();
+        //Character
+        char ch=s.charAt(0);
+
+        System.out.println("Integer types => "+b+" "+sh+" "+n+" "+l);
+        System.out.println("Floating point types => "+f+" "+d);
+        System.out.println("String and character types => "+s+" "+ch);
+    }
+}
