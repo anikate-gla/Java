90a94935e8c2edf6450fb4fd4cbf9282a0c3dc31
anikateagrawal <anikateag7316@gmail.com>
2025-07-12 13:51:45 +0530

    commit

---
+++ For_Loop.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class For_Loop {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        // print number upto n
+        int n=sc.nextInt();
+        for (int i=0;i<n;i++){
+            System.out.println(i);
+        }
+    }
+}
