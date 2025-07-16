e207b37d69ffa429e13373e24e27f9cd84cf8a2c
anikateagrawal <anikateag7316@gmail.com>
2025-07-16 10:53:37 +0530

    commit

---
+++ while_loop.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class while_loop {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        int i=1;
+        while (i<11){
+            System.out.println(num+"*"+i+" = "+(num*i));
+            i++;
+        }
+
+    }
+}
